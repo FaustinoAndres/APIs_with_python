7ee070d970a3de6c8670e74fc09fe1c58233674b
Faustino Correa <faustino.correa.m@gmail.com>
2022-07-06 20:21:33 -0400

    02 args

---
+++ 02_args/main.py
@@ -0,0 +1,14 @@
+from email import contentmanager
+from urllib import response
+import requests
+
+if __name__ == '__main__':
+
+    url = 'http://httpbin.org/get'
+    args = { 'nombre': 'Faustino' }
+
+    response = requests.get(url, params=args)
+
+    if response.status_code == 200:
+        content = response.content
+        print(content)
